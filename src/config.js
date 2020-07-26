d1113ee7b7d75b4ce64e62a973ffc8132f1e63ec
Asad Iqbal <asad.dsoft@gmail.com>
2020-07-25 20:47:11 -0500

    react blog uisng issues with label -blog

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "MDU1MmMwYjgyYmJlZDU0ZDc5OTRmMWU5ODRjMjBmMWIyOTYyMGE3Mg==",
+
+  // Your Github UserName
+  githubUserName: "saadqbal",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "saadqbal.github.io",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.839ea0c1e669ac77',
+  OAuthSecret: 'a6a9d2419fbb048819150ce5d8eb61f49cf00f26',
+
+  // Your Personal Blog Title
+  title : "Asad Iqbal" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Full Stack Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
