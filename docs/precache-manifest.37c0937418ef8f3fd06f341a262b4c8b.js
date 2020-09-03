1efb6d8593420dc8779dc677bc0b9c3cce4a8fa8
Asad Iqbal <asad.dsoft@gmail.com>
2020-09-03 14:58:41 -0500

    created the docs

---
+++ docs/precache-manifest.37c0937418ef8f3fd06f341a262b4c8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04f915db0a99ce031dc05ac8ae947c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69d1698447cd46fe2b1",
+    "url": "/static/css/2.ebe4ca9a.chunk.css"
+  },
+  {
+    "revision": "8ea4233278a6d271cffa",
+    "url": "/static/css/main.15961fd0.chunk.css"
+  },
+  {
+    "revision": "c69d1698447cd46fe2b1",
+    "url": "/static/js/2.30c82458.chunk.js"
+  },
+  {
+    "revision": "b5394eaa1e18d173675f1f0c5e869853",
+    "url": "/static/js/2.30c82458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea4233278a6d271cffa",
+    "url": "/static/js/main.d9057902.chunk.js"
+  },
+  {
+    "revision": "d9ca2d739af42f1d2ff7",
+    "url": "/static/js/runtime-main.78b4016f.js"
+  }
+]);
